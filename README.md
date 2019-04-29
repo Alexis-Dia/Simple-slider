fada9fbe9c40875be09b44e160ed9adb66372882
Alexis-Dk <alexeydruzik@inbox.ru>
2019-04-29 19:57:02 +0300

    fix(slider): Added fade effect.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 # https://www.figma.com/file/JvyJUEDGwmMYovNB08WRDesJ/2-variant?node-id=0%3A1
 
 # https://www.figma.com/file/cLKaUMnijsSFzgTBlhKEM1uo/2-проект?node-id=0%3A1
+
+# http://css3.bradshawenterprises.com/cfimg/
+
+# https://codepen.io/herrberk/pen/ZOLJmy
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -10,27 +10,12 @@ const fadeStyles = {
     "-o-transition": "opacity 1s ease-in-out",
     "transition": "opacity 1s ease-in-out",
     "opacity":1,
-    "-ms-filter":"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)",
-    "filter": "alpha(opacity=0)",
+    "-ms-filter":"progid:DXImageTransform.Microsoft.Alpha(Opacity=1)",
+    "filter": "alpha(opacity=1)",
     "animation": "zoomin 1s ease-in",
-    "-webkit-animation": "zoomin 1s ease-in",
-    "transition": "all 1s ease-in",
-    "overflow": "hidden",
-    "width":"100%",
-    "height":"100%",
 };
 
 const noFadeStyles = {
-/*    "position":"absolute",
-    "left":0,
-    "-webkit-transition": "opacity 1s ease-in-out",
-    "-moz-transition": "opacity 1s ease-in-out",
-    "-o-transition": "opacity 1s ease-in-out",
-    "transition": "opacity 1s ease-in-out",
-    "opacity":0,
-    "-ms-filter":"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)",
-    "filter": "alpha(opacity=0)",
-    //"animation": "zoomin 1s ease-in",*/
 };
 
 class Slider extends React.Component {
@@ -41,28 +26,16 @@ class Slider extends React.Component {
     };
 
     componentDidMount= ()=> {
-        console.log("this.props.images.length")
         this.setState({ length: this.props.images.length });
     }
 
-    dotClicked = (e, index) => {
-        console.log("index = ", index)
-        this.setState({ fadeStyle: 1 });
-        setTimeout(() => {
-            console.log("111", index)
-            this.setState({ fadeStyle: 0, curIndex: index });
-        }, 1);
-    };
-
     nextIndex = () => {
-        console.log("1")
         if (this.state.curIndex >= 1) {
             this.setState({ fadeStyle: 1, curIndex: this.state.curIndex - 1 });
         }
     };
 
     prevIndex = () => {
-        console.log("2")
         if (this.state.curIndex < this.state.length - 1) {
             this.setState({ fadeStyle: 1, curIndex: this.state.curIndex + 1 });
         }
@@ -81,20 +54,9 @@ class Slider extends React.Component {
                         )}
                     </div>
                 </div>
-{/*                <p id="cf7_controls">
-                    <span onClick={(e) => {
-                        this.dotClicked(e, 0)
-                    }} className="selected">Image 1</span>
-                    <span onClick={(e) => {
-                        this.dotClicked(e, 1)
-                    }}>Image 2</span>
-                    <span>Image 3</span>
-                    <span>Image 4</span>
-                </p>*/}
             </ReactScrollWheelHandler>
         );
     }
 }
 
 export default Slider;
-
